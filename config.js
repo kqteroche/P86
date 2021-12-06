a1c9bb67d55be22d16e4e328f52d2a9c0d4caa0f
kqteroche <69346084+kqteroche@users.noreply.github.com>
2021-12-06 12:48:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCbGJbnNO6yvsY42mAn-5N_NkdIGTxTcac",
+  authDomain: "spectagram-9be8f.firebaseapp.com",
+  projectId: "spectagram-9be8f",
+  storageBucket: "spectagram-9be8f.appspot.com",
+  messagingSenderId: "426949059898",
+  appId: "1:426949059898:web:96c6f2e4a0d211373c9c03"
+};
